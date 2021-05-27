5199ea024445baba98ce2b3957d7ad9f8371ea1a
Valdir Cezar <valdircezar312@gmail.com>
2021-05-26 22:39:53 -0300

    classe Pessoa iniciada

--- src/main/java/com/valdir/hp/HpApplication.java
+++ src/main/java/com/valdir/hp/HpApplication.java
@@ -3,6 +3,8 @@ package com.valdir.hp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdir.hp.model.Pessoa;
+
 @SpringBootApplication
 public class HpApplication {
 
---
+++ src/main/java/com/valdir/hp/model/Pessoa.java
@@ -0,0 +1,102 @@
+package com.valdir.hp.model;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String senha;
+	private LocalDate dataCriacao;
+
+	public Pessoa() {
+		super();
+		this.dataCriacao = LocalDate.now();
+	}
+
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha, LocalDate dataCriacao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.dataCriacao = LocalDate.now();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
