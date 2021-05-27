25a5c44f8905295b7adf36b86be3b4183a041228
Valdir Cezar <valdircezar312@gmail.com>
2021-05-27 14:34:39 -0300

    Retornando DTO em GET por ID para Tecnico

--- src/main/java/com/valdir/hp/resources/TecnicoResource.java
+++ src/main/java/com/valdir/hp/resources/TecnicoResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.valdir.hp.dtos.TecnicoDTO;
 import com.valdir.hp.model.Tecnico;
 import com.valdir.hp.services.TecnicoService;
 
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 
 }
