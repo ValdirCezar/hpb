832f573a1ccf1dc40291b53e309057dce20f843f
Valdir Cezar <valdircezar312@gmail.com>
2021-05-27 11:54:05 -0300

    TecnicoDTO criado

--- src/main/java/com/valdir/hp/dtos/TecnicoDTO.java
+++ src/main/java/com/valdir/hp/dtos/TecnicoDTO.java
@@ -1,4 +1,4 @@
-package com.valdir.hp.model;
+package com.valdir.hp.dtos;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -6,55 +6,42 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.valdir.hp.enums.Perfil;
 
-@Entity
-public abstract class Pessoa implements Serializable {
+public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
-	@Column(unique = true)
+	@CPF
 	private String cpf;
 
-	@Column(unique = true)
+	@Email
 	private String email;
 	private String senha;
 	private LocalDateTime dataCriacao;
-
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "PERFIS")
 	private Set<Integer> perfis = new HashSet<>();
 
-	public Pessoa() {
+	public TecnicoDTO() {
 		super();
 		this.dataCriacao = LocalDateTime.now();
-		addPerfil(Perfil.ROLE_CLIENTE);
 	}
 
-	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+	public TecnicoDTO(Integer id, String nome, @CPF String cpf, @Email String email, String senha,
+			LocalDateTime dataCriacao, Perfil perfil) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.email = email;
 		this.senha = senha;
-		this.dataCriacao = LocalDateTime.now();
-		addPerfil(Perfil.ROLE_CLIENTE);
+		this.dataCriacao = dataCriacao;
+		addPerfil(perfil);
 	}
 
 	public Integer getId() {
@@ -111,31 +98,6 @@ public abstract class Pessoa implements Serializable {
 
 	public void addPerfil(Perfil perfil) {
 		this.perfis.add(perfil.getCod());
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pessoa other = (Pessoa) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
