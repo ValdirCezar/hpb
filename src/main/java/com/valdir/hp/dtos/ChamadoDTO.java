c646e806956248aafde966fe1d13d08bba662e4a
Valdir Cezar <valdircezar312@gmail.com>
2021-05-27 14:13:10 -0300

    ChamadoDTO criado

---
+++ src/main/java/com/valdir/hp/dtos/ChamadoDTO.java
@@ -0,0 +1,115 @@
+package com.valdir.hp.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.valdir.hp.model.Chamado;
+
+public class ChamadoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbertura;
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataFechamento;
+
+	private Integer prioridade;
+	private Integer status;
+	private String titulo;
+	private String descricao;
+	private Integer tecnico;
+	private Integer cliente;
+
+	public ChamadoDTO() {
+		super();
+	}
+
+	public ChamadoDTO(Chamado obj) {
+		super();
+		this.id = obj.getId();
+		this.dataAbertura = obj.getDataAbertura();
+		this.dataFechamento = obj.getDataAbertura();
+		this.prioridade = obj.getPrioridade().getCod();
+		this.status = obj.getStatus().getCod();
+		this.titulo = obj.getTitulo();
+		this.descricao = obj.getDescricao();
+		this.tecnico = obj.getTecnico().getId();
+		this.cliente = obj.getCliente().getId();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDate dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDate getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDate dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Integer tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Integer getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
+	}
+
+}
