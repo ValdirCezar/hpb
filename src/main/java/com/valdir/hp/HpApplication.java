736ac0240eca828e2f00f53486f332a615c16f5b
Valdir Cezar <valdircezar312@gmail.com>
2021-05-26 22:18:31 -0300

    projeto iniciado

---
+++ src/main/java/com/valdir/hp/HpApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdir/hp/HpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.hp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
