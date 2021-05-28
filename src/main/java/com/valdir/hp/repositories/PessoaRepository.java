2aca899afdb7ecfcd5f72d3fd6795b3da1f79540
Valdir Cezar <valdircezar312@gmail.com>
2021-05-27 21:26:41 -0300

    Validando CPF e E-mail no update

--- src/main/java/com/valdir/hp/repositories/PessoaRepository.java
+++ src/main/java/com/valdir/hp/repositories/PessoaRepository.java
@@ -1,5 +1,7 @@
 package com.valdir.hp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.valdir.hp.model.Pessoa;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
--- src/main/java/com/valdir/hp/services/TecnicoService.java
+++ src/main/java/com/valdir/hp/services/TecnicoService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdir.hp.dtos.TecnicoDTO;
+import com.valdir.hp.model.Pessoa;
 import com.valdir.hp.model.Tecnico;
+import com.valdir.hp.repositories.PessoaRepository;
 import com.valdir.hp.repositories.TecnicoRepository;
 import com.valdir.hp.services.exceptions.DataIntegrityViolationException;
 import com.valdir.hp.services.exceptions.ObjectNotFoundException;
@@ -17,6 +19,9 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -29,12 +34,22 @@ public class TecnicoService {
 	}
 
 	public Tecnico create(TecnicoDTO objDTO) {
+		
+		if(findByCpf(objDTO.getCpf()) != null) {
+			throw new DataIntegrityViolationException("CPF " + objDTO.getCpf() + " já cadastrado no sistema!");
+		}
+		
+		if(findByEmail(objDTO.getEmail()) != null) {
+			throw new DataIntegrityViolationException("E-mail " + objDTO.getEmail() + " já cadastrado no sistema!");
+		}
+		
 		return repository.save(new Tecnico(objDTO));
 	}
 
 	public Tecnico update(Integer id, TecnicoDTO objDTO) {
 		objDTO.setId(id);
 		Tecnico old = findById(id);
+		validaUpdate(objDTO);
 		old = new Tecnico(objDTO);
 		return repository.save(old);
 	}
@@ -48,5 +63,25 @@ public class TecnicoService {
 		} else {
 			repository.deleteById(id);
 		}
+	}
+	
+	public Optional<Pessoa> findByCpf(String cpf) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(cpf);
+		return obj.isPresent() ? obj : null;
+	}
+	
+	public Optional<Pessoa> findByEmail(String email) {
+		Optional<Pessoa> obj = pessoaRepository.findByEmail(email);
+		return obj.isPresent() ? obj : null;
+	}
+	
+	private void validaUpdate(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) 
+			throw new DataIntegrityViolationException("CPF " + objDTO.getCpf() + " já cadastrado no sistema!");
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) 
+			throw new DataIntegrityViolationException("E-mail " + objDTO.getEmail() + " já cadastrado no sistema!");
 	}
 }
